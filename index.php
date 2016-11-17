da6a5e35c58204f470b105f59dca01553e34a668
Robert Sefer <rsefer@gmail.com>
2016-11-17 13:21:21 -0600

    Basic index

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php get_header(); ?>
 
-<?php the_content(); ?>
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<section class="page-header index-header" <?php if (has_post_thumbnail()) { echo ' style="background-image: url(\''; the_post_thumbnail_url(); echo '\');"'; } ?>">
+  <div class="page-header-meta">
+    <div class="grid-wrapper">
+      <div class="pure-g">
+        <div class="pure-u-lg-1-8"></div>
+        <div class="pure-u-lg-3-4">
+          <h1 class="title-boxed"><?php the_title(); ?></h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="content-main about-main">
+  <div class="grid-wrapper">
+    <div class="pure-g">
+      <div class="pure-u-lg-1-8"></div>
+      <div class="pure-u-lg-3-4">
+        <?php the_content(); ?>
+      </div>
+    </div>
+  </div>
+</section>
+<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
